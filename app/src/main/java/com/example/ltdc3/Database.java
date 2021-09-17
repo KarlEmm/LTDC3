ec0cd96b44a9aa6e06f816fc6fb452be6ab49b21
Karl-Emmanuel Bastarache <karl.bast@gmail.com>
2021-09-17 13:41:34 -0400

    Recycler View Fully Operational. Started Facebook integration

--- app/src/main/java/com/example/ltdc3/Database.java
+++ app/src/main/java/com/example/ltdc3/Database.java
@@ -29,10 +29,20 @@ public class Database {
     public Database() {
         ArrayList<Comment> comments = new ArrayList<>();
         User u1 = new User("karl.bast@gmail.com", "Bastarache", "Karl-Emmanuel", "Séminaire Saint-Joseph", "password", R.drawable.teenager2);
-        Comment c1 = new Comment(u1, "This looks very good! Can I have a share tomorrow? :')", "1h");
-        comments.add(c1);
-        FeedData fd1 = new FeedData(idSingleton++, R.drawable.spaghetti, "Yummy Spaghetti", "2021/09/15", u1, 3, comments);
+        User u2 = new User("lisa.marier@gmail.com", "Marier", "Lisa", "Séminaire Saint-Joseph", "password", R.drawable.teenager1);
         users.add(u1);
+        users.add(u2);
+
+        Comment c1 = new Comment(u2, "This looks very good! Can I have a share tomorrow? :')", "1h");
+        comments.add(c1);
+
+        FeedData fd1 = new FeedData(idSingleton++, R.drawable.spaghetti, "Yummy Spaghetti", "2021/09/15", u1, 3, comments);
         feedData.add(fd1);
+
+        c1 = new Comment(u1, "What recipe did you follow? This looks amazing.", "34min");
+        comments.clear();
+        comments.add(c1);
+        FeedData fd2 = new FeedData(idSingleton++, R.drawable.gazpacho, "My Summer Gazpacho", "2021/09/15", u2, 10, comments);
+        feedData.add(fd2);
     }
 }
