1e50d8515bb4c543f67edab6e7bb78222151d858
william <william_martineau@hotmail.com>
2021-09-17 10:43:44 -0400

    commencement navigation bar

---
+++ app/src/main/java/com/example/ltdc3/Chef.java
@@ -0,0 +1,27 @@
+package com.example.ltdc3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Chef#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Chef extends Fragment {
+
+    public Chef(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chef, container, false);
+    }
+}
--- app/src/main/java/com/example/ltdc3/MainActivity.java
+++ app/src/main/java/com/example/ltdc3/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.ltdc3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+    Home homeFrag = new Home();
+    Challenge challengeFrag = new Challenge();
+    Leaderboard leaderboardFrag = new Leaderboard();
+    Chef chefFrag = new Chef();
+
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, homeFrag).commit();
+                return true;
+
+            case R.id.challenges:
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, challengeFrag).commit();
+                return true;
+
+            case R.id.leaderboard:
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, leaderboardFrag).commit();
+                return true;
+
+            case R.id.chef:
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, chefFrag).commit();
+                return true;
+        }
+        return false;
     }
 }
