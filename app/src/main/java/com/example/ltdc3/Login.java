009da541338890d89ae7378d6ce359aacc21c9bc
william <william_martineau@hotmail.com>
2021-09-18 19:02:31 -0400

    fix null reference bug

--- app/src/main/java/com/example/ltdc3/Login.java
+++ app/src/main/java/com/example/ltdc3/Login.java
@@ -2,13 +2,26 @@ package com.example.ltdc3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        createAccountButton = (Button)findViewById(R.id.create_account_button);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(),CreateAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ltdc3/MainActivity.java
+++ app/src/main/java/com/example/ltdc3/MainActivity.java
@@ -1,40 +1,25 @@
 package com.example.ltdc3;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class MainActivity extends AppCompatActivity {
+    Button quickFixButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.button2);
-        btn.setOnClickListener(new OnClickListener() {
+        quickFixButton = (Button)findViewById(R.id.button2);
+        quickFixButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myIntent = new Intent(MainActivity.this, Login.class);
                 MainActivity.this.startActivity(myIntent);
-            }
-        });
-        setContentView(R.layout.activity_create_account);
-        Button btn1 = (Button)findViewById(R.id.button3);
-        btn1.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent1 = new Intent (MainActivity.this,CreateAccount.class);
-                MainActivity.this.startActivity(myIntent1);
             }
         });
 //        BottomNavigationView navView = findViewById(R.id.nav_view);
