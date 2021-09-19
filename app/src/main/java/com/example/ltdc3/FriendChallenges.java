0c1842510b4a99772c540803cf31f7fef58f0832
Carl <corteocarl@gmail.com>
2021-09-19 00:14:52 -0400

    Added friend suggestions challenge friend

--- app/src/main/java/com/example/ltdc3/FriendChallenges.java
+++ app/src/main/java/com/example/ltdc3/FriendChallenges.java
@@ -1,12 +1,21 @@
 package com.example.ltdc3;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,15 +23,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FriendChallenges extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View mView;
 
     public FriendChallenges() {
         // Required empty public constructor
@@ -31,34 +32,42 @@ public class FriendChallenges extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment FriendChallenges.
      */
     // TODO: Rename and change types and number of parameters
-    public static FriendChallenges newInstance(String param1, String param2) {
+    public static FriendChallenges newInstance() {
         FriendChallenges fragment = new FriendChallenges();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend_challenges, container, false);
+        mView = inflater.inflate(R.layout.fragment_friend_challenges, container, false);
+        setupSuggestions();
+
+        return mView;
+    }
+
+    private void setupSuggestions() {
+        AutoCompleteTextView nameInput = mView.findViewById(R.id.challengeNameInput);
+        String[] suggestions = new String[2];
+        Context appContext = getActivity();
+
+        ArrayList<String> suggestedNames = new ArrayList<String>();
+        Database db = (Database)appContext.getApplicationContext();
+
+        for(User user : db.getUsers()) {
+            suggestedNames.add(user.firstname + " " + user.name);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, suggestedNames);
+        nameInput.setAdapter(adapter);
     }
 }
