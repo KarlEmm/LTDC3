7e141077cf910bac8a5b5b6911df3c041ffece0d
lazureetienne <47014155+lazureetienne@users.noreply.github.com>
2021-09-20 01:24:59 -0400

    added spinner functionality on signup screen

--- app/src/main/java/com/example/ltdc3/CreateAccount.java
+++ app/src/main/java/com/example/ltdc3/CreateAccount.java
@@ -3,6 +3,11 @@ package com.example.ltdc3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        // you need to have a list of data that you want the spinner to display
+        List<String> spinnerArray =  new ArrayList<String>();
+        spinnerArray.add("Pensionnat St-Joseph");
+        spinnerArray.add("Collège Durocher");
+        spinnerArray.add("Collège Jean-de-Brébeuf");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner);
+        sItems.setAdapter(adapter);
     }
 }
